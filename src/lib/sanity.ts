57e58cd7b8a6665667129090482e480e7fcfbdfa
Victor Meneses <vmeneses782000@gmail.com>
2026-04-25 14:01:15 -0500

    fix: fetch fresh Sanity content during revalidation

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -6,7 +6,7 @@ export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-04-24",
-  useCdn: true,
+  useCdn: false,
 });
 
 const builder = imageUrlBuilder(sanityClient);
